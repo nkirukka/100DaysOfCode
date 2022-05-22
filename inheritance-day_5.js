4ede1206564afb830c3dd0ff5a0b34578406e1bf
Nkiruka Ebele <specialebele@gmail.com>
2022-05-22 16:27:32 +0100

    Add files for day 4 and 5

---
+++ inheritance-day_5.js
@@ -0,0 +1,27 @@
+
+/*In this challenge, we practice implementing inheritance 
+and use JavaScript prototypes to add a new method to an 
+existing prototype. */
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+Rectangle.prototype.area = function (){
+    return this.w * this.h;
+}
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle{
+    constructor (side){
+        super(side);
+        this.h = side;
+        this.w = side;
+        
+    }
+}
--- secondLargest-day_3.js
+++ secondLargest-day_3.js
@@ -1,12 +1,5 @@
 
 
-// const secondLar = (nums) => {
-//     let sort = nums.sort(function (a, b) { return (a - b) });
-//     console.log(sort[sort.length - 2])
-// }
-
-// secondLar([8,2,35,3,1,8,7])
-
 function getSecondLargest(nums) {
     const max = Math.max(...nums)
     return Math.max(...nums.filter(x => x !== max))
